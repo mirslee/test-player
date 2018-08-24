0ee12081a569201ee13ffc6cb09c4dce96d91b56
lixiaopeng <lixiaopeng@wondershare.cn>
2018-08-24 17:58:25 +0800

    555

---
+++ MxCore/MxSynchronize.cpp
@@ -0,0 +1,9 @@
+//
+//  MxSynchronize.cpp
+//  MxCore
+//
+//  Created by sz17112850M01 on 2018/8/24.
+//  Copyright © 2018年 lixiaopeng. All rights reserved.
+//
+
+#include "MxSynchronize.hpp"
---
+++ MxCore/MxSynchronize.h
@@ -0,0 +1,14 @@
+//
+//  MxSynchronize.hpp
+//  MxCore
+//
+//  Created by sz17112850M01 on 2018/8/24.
+//  Copyright © 2018年 lixiaopeng. All rights reserved.
+//
+
+#ifndef MxSynchronize_hpp
+#define MxSynchronize_hpp
+
+#include <stdio.h>
+
+#endif /* MxSynchronize_hpp */
